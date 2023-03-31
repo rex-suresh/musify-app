35e07b2e999c505e9b8da3602ddb1706f0ef84a7
Suresh Kumar Putchala <sputchala@seekasia.com>
2023-03-31 19:32:50 +0530

    fix api_host issue

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -3,4 +3,4 @@ import Config from 'react-native-config';
 export const androidHost = Config.ANDROID_HOST;
 export const iosHost = Config.IOS_HOST;
 export const env = Config.ENV;
-export const apiUrl = Config.API_URL;
+export const apiUrl = Config.API_HOST;
